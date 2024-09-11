2d375b915143a2207c9c68fd288d96ce21c77b05
marcusaleman <marcos.aleman@digital.justice.gov.uk>
2024-09-11 16:59:05 +0100

    MAP-1459 8 ReportService Scenario getPrisonContentFor coroutines

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsuofdataapi/integration/service/ReportServiceCoroutinesTest.kt
@@ -0,0 +1,92 @@
+package uk.gov.justice.digital.hmpps.hmppsuofdataapi.integration.service
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import uk.gov.justice.digital.hmpps.hmppsuofdataapi.model.ReportDetail
+import uk.gov.justice.digital.hmpps.hmppsuofdataapi.repository.ReportRepository
+import uk.gov.justice.digital.hmpps.hmppsuofdataapi.repository.ReportSummaryRepository
+import uk.gov.justice.digital.hmpps.hmppsuofdataapi.service.ReportService
+import uk.gov.justice.hmpps.kotlin.sar.HmppsSubjectAccessRequestContent
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class ReportServiceCoroutinesTest {
+
+  private lateinit var reportRepository: ReportRepository
+  private lateinit var reportSummaryRepository: ReportSummaryRepository
+  private lateinit var reportServiceMock: ReportService
+
+  @InjectMocks
+  private lateinit var reportService: ReportService
+
+  @BeforeEach
+  fun setUp() {
+    reportRepository = mock(ReportRepository::class.java)
+    reportSummaryRepository = mock(ReportSummaryRepository::class.java)
+
+    reportService = ReportService(reportRepository, reportSummaryRepository)
+    reportServiceMock = mock(ReportService::class.java)
+  }
+
+  val offenderNumber = "G1234A"
+  val offenderNumberB = "G1234AB"
+
+  val report1 = buildReport(1, offenderNumber)
+  val report2 = buildReport(2, offenderNumberB)
+  val fromDate = LocalDate.of(2024, 9, 1)
+  val toDate = LocalDate.of(2024, 9, 30)
+
+  private fun buildReport(
+    id: Long,
+    offenderNumber: String,
+  ): ReportDetail {
+    return ReportDetail(
+      id, "{}",
+      "user_id",
+      1,
+      1234,
+      LocalDateTime.now(),
+      "IN_PROGRESS",
+      null,
+      offenderNumber,
+      "reporter_name",
+      LocalDateTime.of(2024, 1, 1, 14, 0),
+      "MDI",
+      LocalDateTime.now(),
+      null,
+    )
+  }
+
+  @Test
+  fun `test getPrisonContentFor returns correct reports coroutines runTest`() = runTest {
+    val listReportDetail = listOf(report1, report2)
+
+    val expectedHmppsSubjectAccessRequestContent = HmppsSubjectAccessRequestContent(
+      content = listOf(report1, report2).map {
+        it.toDto(
+          includeStatements = true,
+          includeFormResponse = true,
+        )
+      },
+    )
+
+    `when`(
+      reportRepository.findAllByOffenderNoAndIncidentDateBetween(
+        offenderNumber,
+        fromDate.atStartOfDay(),
+        toDate.atStartOfDay(),
+      ),
+    ).thenReturn(listOf(report1, report2))
+
+    `when`(reportServiceMock.getReportsByOffenderNumberAndDateWindow(offenderNumber, fromDate, toDate)).thenReturn(listReportDetail)
+    `when`(reportServiceMock.getPrisonContentFor(offenderNumber, fromDate, toDate)).thenReturn(expectedHmppsSubjectAccessRequestContent)
+
+    val gotHmppsSubjectAccessRequestContent = reportService.getPrisonContentFor(offenderNumber, fromDate, toDate)
+
+    assertEquals(gotHmppsSubjectAccessRequestContent, expectedHmppsSubjectAccessRequestContent)
+  }
+}
